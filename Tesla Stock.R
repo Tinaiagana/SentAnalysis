d326fb024e5915b3eb9ab1f31ef94cee415aaf8c
Tina <tinatiniagana@gmail.com>
2019-03-18 23:54:20 +0100

    Sent Analysis Class Code, keys removed and the code is clearer

--- Tesla Stock.R
+++ Tesla Stock.R
@@ -2,26 +2,37 @@ library(twitteR)
 library(tm)
 library(SnowballC)
 library(syuzhet)
-consumer_key <- "ZLX0zDj47f35gmRlPDmyxqLuJ"
-consumer_secret<- "aWawo7GN7SJxFS1X8NBrJDpe4UuOZicQUzVEtf23AK6D6JJUr7"
-access_token <- "107991817-ekKpeILEER2GrZaIr3H51uBMsTDYIL6CTtAi2xiD"
-access_secret <- "iHlVgWV6TPF20AlmqZaAUyGDvCULXEUyMIr0L1jCRWwzA"
+library(tidyverse)
+consumer_key <- xxx
+consumer_secret<-xxx
+access_token <- xxx
+access_secret <- xxx
 setup_twitter_oauth(consumer_key ,consumer_secret,access_token ,access_secret)
 
 tweets.EM <- searchTwitter(searchString = "Tesla + Stock -filter:retweets", n=2000,
-            since ="2019-03-03", until= "2019-03-10", retryOnRateLimit = 80)
+            since ="2019-03-12", retryOnRateLimit = 80)
 
 n.tweets.EM <- length(tweets.EM)
+tweets.EM <- strip_retweets(tweets.EM)
 
 tweets.EM.df <- twListToDF(tweets.EM)
 head(tweets.EM.df)
 head(tweets.EM.df$text)
 
+#CLeanup data!
+
 tweets.EM.df2 <- gsub("http.*","",tweets.EM.df$text)
 tweets.EM.df2 <- gsub("http.*","",tweets.EM.df2)
 tweets.EM.df2 <- gsub("#.*","",tweets.EM.df2)
 tweets.EM.df2 <- gsub("@.*","",tweets.EM.df2)
+tweets.EM.df2 <- gsub("@\\w+", "", tweets.EM.df2)
+tweets.EM.df2 <- gsub('[[:punct:]]', '', tweets.EM.df2)
+tweets.EM.df2 <- gsub('[[:cntrl:]]', '', tweets.EM.df2)
+tweets.EM.df2 <- gsub("[[:digit:]]", "", tweets.EM.df2)
+tweets.EM.df2 <- gsub("[ \t]{2,}", "", tweets.EM.df2)
+tweets.EM.df2 <- gsub("^\\s+|\\s+$", "", tweets.EM.df2)
 head(tweets.EM.df2)
+
 
 #getting sentiment score for each tweet
 word.EM.df <- as.vector(tweets.EM.df2)
@@ -39,6 +50,7 @@ head(most.positive)
 most.negative <- word.EM.df[sent.value == min(sent.value)]
 head(most.negative)
 positive.tweets <- word.EM.df[sent.value > 0]
+
 head(positive.tweets)
 
 negative.tweets <- word.EM.df[sent.value < 0]
@@ -48,9 +60,6 @@ neutral.tweets <- word.EM.df[sent.value == 0]
 head(neutral.tweets)
 
 category_senti <- ifelse(sent.value < 0, "Negative", ifelse(sent.value > 0, "Positive", "Neutral"))
-#ERRORRR
-category_senti2 <- cbind(tweets.EM,category_senti,senti) > head(category_senti2)
-tweets category_senti senti
 
 head(category_senti)
 table(category_senti)
